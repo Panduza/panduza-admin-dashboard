f1449979048af6d18a6386287c5b01ef8b9288d5
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2023-09-01 08:50:33 +0200

    create an easy way to update the local conf when developing

---
+++ local/update.sh
@@ -0,0 +1,10 @@
+
+# ===================================================================
+# PARAMETERS
+PYTHON_VENV_PATH=/usr/local/bin/panduza/venv
+
+# ===================================================================
+
+
+sudo ${PYTHON_VENV_PATH}/bin/pip3 install ./
+
